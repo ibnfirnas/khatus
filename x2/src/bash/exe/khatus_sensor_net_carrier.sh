fca6896b9e697c9b74cda916f15cbbbc1e55865e
Siraaj Khandkar <siraaj@khandkar.net>
2019-03-06 14:43:22 -0500

    Ignore /sys/classa/net/$iface/carrier access errors
    which begin when interface is disabled.

--- x2/src/bash/exe/khatus_sensor_net_carrier.sh
+++ x2/src/bash/exe/khatus_sensor_net_carrier.sh
@@ -2,5 +2,5 @@
 
 for interface in $(ls /sys/class/net)
 do
-    printf "%s|%d\n" $interface $(cat /sys/class/net/$interface/carrier)
+    printf "%s|%d\n" $interface $(cat /sys/class/net/$interface/carrier 2> /dev/null)
 done
