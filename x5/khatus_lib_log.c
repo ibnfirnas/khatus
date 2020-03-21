c7262f37a3abbd4931eefb8cd2c9f9408b76750c
Siraaj Khandkar <siraaj@khandkar.net>
2020-03-21 16:54:16 -0400

    Fix fatality trigger

--- x5/khatus_lib_log.c
+++ x5/khatus_lib_log.c
@@ -5,13 +5,3 @@
 #include "khatus_lib_log.h"
 
 LogLevel _khatus_lib_log_level = Debug;
-
-void
-_fatal(char *fmt, ...) {
-	va_list ap;
-
-	va_start(ap, fmt);
-	fprintf(stderr, fmt, ap);
-	va_end(ap);
-	exit(EXIT_FAILURE);
-}
--- x5/khatus_lib_log.h
+++ x5/khatus_lib_log.h
@@ -2,7 +2,7 @@
 #define info(...)  if (_khatus_lib_log_level >= Info ) {fprintf(stderr, "[info] "  __VA_ARGS__); fflush(stderr);}
 #define warn(...)  if (_khatus_lib_log_level >= Warn ) {fprintf(stderr, "[warn] "  __VA_ARGS__); fflush(stderr);}
 #define error(...) if (_khatus_lib_log_level >= Error) {fprintf(stderr, "[error] " __VA_ARGS__); fflush(stderr);}
-#define fatal(...) _fatal("[fatal] " __VA_ARGS__)
+#define fatal(...)                                     {fprintf(stderr, "[fatal] " __VA_ARGS__); exit(EXIT_FAILURE);}
 
 typedef enum LogLevel {
 	Nothing,
@@ -11,7 +11,5 @@ typedef enum LogLevel {
 	Info,
 	Debug
 } LogLevel;
-
-void _fatal(char *, ...);
 
 LogLevel _khatus_lib_log_level;
